0027b2c97834c8efd9f85a6e37be38393a729caf
usuario <usuario@HOME_PC>
2022-08-03 20:18:02 -0300

    teste do model e utils

---
+++ Mercado/src/view/Teste.java
@@ -0,0 +1,16 @@
+package view;
+
+import model.Produto;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		Produto ps4 = new Produto("Playstatio 4", 1789.40);
+		Produto xbox = new Produto("Xbox 360", 1699.00);
+		
+		System.out.println(ps4);
+		System.out.println(xbox);
+
+	}
+
+}
